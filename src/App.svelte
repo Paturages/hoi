baea6cd9db88a29e77c629dd7dcdbe660eaf06e9
Paturages <c.ingouff@gmail.com>
2023-06-16 15:59:30 +0200

    Add local storage click counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import * as Tone from "tone";
-  import { shuffle } from "./utils";
-  import Sprite from "./components/Sprite.svelte";
 
+  import { shuffle } from "./utils";
+  import { getCount, incrementCount } from "./utils/counter";
+
+  import Sprite from "./components/Sprite.svelte";
+  import Counter from "./components/Counter.svelte";
+
+  let count = getCount();
+  let loaded = false;
+
+  // Sound variables
   const SOUND_COUNT = 16;
   const soundObj: Record<string, string> = {};
-  let loaded = false;
   let soundUrls: string[] = [];
   let soundQueue: string[];
   let cycleFirstSound, currentSound;
   let tonePlayers: Tone.Players;
+
+  // Sprite variables
+  const SPRITE_COUNT = 15;
+  let spriteUrls: string[] = [];
+  let spriteQueue: string[];
+  let cycleFirstSprite, currentSprite;
   
   Tone.getContext().dispose();
   const toneContext = new Tone.Context({
@@ -22,13 +35,23 @@
 
   const handleClick = () => {
     if (!loaded) return;
+
+    // Cycle sounds
     tonePlayers.player(currentSound).start(0);
     soundQueue.unshift(currentSound);
     currentSound = soundQueue.pop();
     if (currentSound == cycleFirstSound) shuffle(soundQueue);
+
+    // Cycle sprites
+    spriteQueue.unshift(currentSprite);
+    currentSprite = spriteQueue.pop();
+    if (currentSprite == cycleFirstSprite) shuffle(spriteQueue);
+
+    count = incrementCount();
   }
 
   onMount(async () => {
+    // Load sound bites
     soundUrls = await Promise.all(new Array(SOUND_COUNT).fill(0).map(async (_, i) => {
       const fileName = `${i+1}`.padStart(3, '0');
       const ogg = await import(`./assets/sounds/common/${fileName}.ogg`);
@@ -39,6 +62,16 @@
     currentSound = soundQueue.pop();
     cycleFirstSound = currentSound;
     tonePlayers = new Tone.Players(soundObj, () => { loaded = true; }).chain(toneContext.destination);
+
+    // Load sprites
+    spriteUrls = await Promise.all(new Array(SPRITE_COUNT).fill(0).map(async (_, i) => {
+      const fileName = `${i+1}`.padStart(3, '0');
+      const img = await import(`./assets/sprites/${fileName}.png`);
+      return img.default;
+    }));
+    spriteQueue = shuffle(spriteUrls.slice());
+    currentSprite = spriteQueue.pop();
+    cycleFirstSprite = currentSprite;
   });
 </script>
 
@@ -46,14 +79,19 @@
   {#each soundUrls as src}
     <link rel="preload" as="audio" href={src} />
   {/each}
+  {#each spriteUrls as src}
+    <link rel="preload" as="image" href={src} />
+  {/each}
 </svelte:head>
 
 <div class="bg" />
-<Sprite onClick={handleClick} />
+<button class="hoi-button" on:click={handleClick} />
+<Counter {count} />
+<Sprite src={currentSprite} />
 
 <style>
   .bg {
-    z-index: 0;
+    z-index: -10;
     position: absolute;
     top: 0;
     bottom: 0;
@@ -61,5 +99,20 @@
     right: 0;
     background: url('./assets/bg.svg');
     opacity: .2;
+  }
+  .hoi-button {
+    z-index: 10;
+    border: none;
+    outline: none;
+    appearance: none;
+    cursor: pointer;
+    background: transparent;
+    padding: 0;
+    margin: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
   }
 </style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let count = 0;
+</script>
+
+<div class="container">
+  <div class="counter">{count}</div>
+</div>
+
+<style>
+  .container {
+    position: relative;
+    z-index: 2;
+    font-family: Comicy, sans-serif;
+    margin-top: 1em;
+    width: 100%;
+    text-align: center;
+  }
+  .counter {
+    z-index: 5;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    font-size: 4em;
+    color: #eca0a3;
+    -webkit-text-stroke: 4px #fff;
+    letter-spacing: 3px;
+  }
+</style>
